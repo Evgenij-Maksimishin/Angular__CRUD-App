411d443664bd09a7f240b2ac3d4a904a345d2d26
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2022-01-09 00:39:08 +0200

    Configured project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { AppRoutingModule } from './app-routing.module';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    AppRoutingModule,
+    LayoutModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
