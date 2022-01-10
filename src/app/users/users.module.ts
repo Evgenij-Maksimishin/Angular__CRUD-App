f5448515a5e90a822c8f195a10ff147596a76244
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2022-01-10 23:37:21 +0200

    Update app

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -7,6 +7,7 @@ import { DeleteUserComponent } from './delete-user/delete-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 
 import {MatListModule} from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -22,7 +23,8 @@ import {MatListModule} from '@angular/material/list';
   ],
   imports: [
     CommonModule,
-    MatListModule
+    MatListModule,
+    RouterModule
   ]
 })
 export class UsersModule { }
