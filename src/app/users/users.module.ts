bd97d5039f8622661a110249d4a63455e48cbd0c
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2022-01-13 01:08:43 +0200

    Update app

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -14,6 +14,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS_FACTORY } from '@angular/material/snack-bar';
+
 
 
 
@@ -37,7 +39,11 @@ import { MatButtonModule } from '@angular/material/button';
     ReactiveFormsModule,
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
+  ],
+  providers: [
+    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS_FACTORY, useValue: { duration: 2500 } }
   ]
 })
 export class UsersModule { }
